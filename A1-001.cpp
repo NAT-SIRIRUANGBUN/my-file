b2b2e72796bec97204e6784df8c373e5c2b5fe30
NAT SIRIRUANGBUN <nat.siriruangbun@gmail.com>
2025-05-28 21:11:51 +0700

    123

---
+++ A1-001.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std ; 
+
+int main () {
+
+    string a, b;
+    getline(cin, a);
+    getline(cin, b);
+
+    cout << "Hello " + a + " " + b + "\n";
+    cout << a[0] << a[1] << b[0] << b[1];
+
+
+}
